5ab00b48d61a8b4eb3dcfa10eac900f0d5eb7ef8
今井 航平 <k-imai@kip.co.jp>
2023-11-09 15:59:59 +0900

    UPDATE

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import textfile
 import glob
-
-
-dir_path = './build/*.html'
-search_in_dir = '<a href="./" class="list-group-item ">'
-replace_in_dir = '<a href="./index.html" class="list-group-item ">'
-
-subdir_path = './build/**/*.html'
-search_in_subdir = '<a href="../" class="list-group-item ">'
-replace_in_subdir = '<a href="../index.html" class="list-group-item ">'
-
-seach_header_in_dir = '<a href="." class="logo">'
-replace_header_in_dir = '<a href="./index.html" class="logo">'
-
-seach_header_in_subdir = '<a href=".." class="logo">'
-replace_header_in_subdir = '<a href="../index.html" class="logo">'
+import settings
 
 
 def replace(directory_path, search_string, replace_string):
@@ -24,10 +10,25 @@ def replace(directory_path, search_string, replace_string):
         print('Replace complete:', file)
 
 
-# build直下の.htmlファイル書換
-replace(dir_path, search_in_dir, replace_in_dir)
-replace(dir_path, seach_header_in_dir, replace_header_in_dir)
+if __name__ == '__main__':
+    # build直下の.htmlファイル書換
+    replace(
+        settings.DIR_PATH,
+        settings.SEARCH_IN_DIR,
+        settings.REPLACE_IN_DIR)
 
-# build/**/の.htmlファイル書換
-replace(subdir_path, search_in_subdir, replace_in_subdir)
-replace(subdir_path, seach_header_in_subdir, replace_header_in_subdir)
+    replace(
+        settings.DIR_PATH,
+        settings.SEARCH_HEADER_IN_DIR,
+        settings.REPLACE_HEADER_IN_DIR)
+
+    # build/**/の.htmlファイル書換
+    replace(
+        settings.SUBDIR_PATH,
+        settings.SEARCH_HEADER_IN_SUBDIR,
+        settings.REPLACE_HEADER_IN_SUBDIR)
+
+    replace(
+        settings.SUBDIR_PATH,
+        settings.SEARCH_HEADER_IN_SUBDIR,
+        settings.REPLACE_HEADER_IN_SUBDIR)
---
+++ settings.py
@@ -0,0 +1,13 @@
+DIR_PATH = './build/*.html'
+SEARCH_IN_DIR = '<a href="./" class="list-group-item ">'
+REPLACE_IN_DIR = '<a href="./index.html" class="list-group-item ">'
+
+SUBDIR_PATH = './build/**/*.html'
+SEARCH_IN_DIR = '<a href="../" class="list-group-item ">'
+REPLACE_IN_SUBdir = '<a href="../index.html" class="list-group-item ">'
+
+SEARCH_HEADER_IN_DIR = '<a href="." class="logo">'
+REPLACE_HEADER_IN_DIR = '<a href="./index.html" class="logo">'
+
+SEARCH_HEADER_IN_SUBDIR = '<a href=".." class="logo">'
+REPLACE_HEADER_IN_SUBDIR = '<a href="../index.html" class="logo">'
